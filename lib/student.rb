39f478da399eedb0395a04225bd21aacbc12742e
Diogo Ide <diogoide@gmail.com>
2018-10-13 20:25:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,20 +44,14 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
 
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end 
+  end
 
   #Method that creates a new instance of student class and then saves it to the database
 
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
-    student 
-  end 
-
-# 1.	  def self.create(name:, album:)
-# 2.	    song = Song.new(name, album)
-# 3.	    song.save
-# 4.	    song
-# 5.	  end
+    student
+  end
 
 end
